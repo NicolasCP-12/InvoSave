bc4d0699f01620915baeb74159617f9fc5ad1518
Fábio Rocha <fabiorocha@Fabios-MacBook-Pro.local>
2025-12-06 17:33:57 +0000

    Navbar styled on main.html

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+<script>
+  tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          fraunces: ["Fraunces", "serif"],
+          manrope: ["Manrope", "sans-serif"],
+        },
+      },
+    },
+  };
+</script>
